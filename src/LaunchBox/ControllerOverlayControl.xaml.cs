eeabae2f113eb532de1e2b9166557936c2e7840a
Trevor Stedman <trevor@seedlegals.com>
2026-07-03 17:04:56 +0100

    Add a user control to allow rendering of controller with a single line of XML

---
+++ src/LaunchBox/ControllerOverlayControl.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace DynamicControls.LaunchBox;
+
+public partial class ControllerOverlayControl : UserControl
+{
+    public ControllerOverlayControl()
+    {
+        InitializeComponent();
+    }
+}
